951c3af6269ef3527d974da9b639458624453559
Дмитрий <paravozikcs@gmail.com>
2021-09-11 10:34:30 +0300

    removed api

--- app/api/users.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify, current_app, url_for, request
-from . import api
-from ..models import User
-
-
-@api.route('/user/<int:id>')
-def get_user(id):
-    user = User.query.get_or_404(id)
-    return jsonify(user.to_json())
